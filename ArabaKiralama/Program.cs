83a6383af2e54bf30f07d2c1742aeab4959125a9
beratbaykan64-cmd <beratbaykan64@gmail.com>
2025-12-24 05:12:44 +0300

    PostgreSQL tarih ve bağlantı havuzu fixlendi

--- ArabaKiralama/Program.cs
+++ ArabaKiralama/Program.cs
@@ -1,6 +1,9 @@
 using ArabaKiralama.Data;
 using Microsoft.EntityFrameworkCore;
 
+// 1. KRÝTÝK DÜZELTME: PostgreSQL Tarih/Saat Hatasýný Çözer (Efe Baba Özel)
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Render'dan gelen gizli veritabaný adresini al
@@ -15,14 +18,15 @@ if (string.IsNullOrEmpty(rawConnectionString))
 }
 else
 {
-    // CANLI (RENDER): PostgreSQL linkini parçalar ve formatýný düzeltir
+    // 2. KRÝTÝK DÜZELTME: CANLI (RENDER) Baðlantý Ayarlarý
     var uri = new Uri(rawConnectionString);
     var db = uri.AbsolutePath.TrimStart('/');
     var user = uri.UserInfo.Split(':')[0];
     var passwd = uri.UserInfo.Split(':')[1];
     var port = uri.Port > 0 ? uri.Port : 5432;
 
-    connectionString = $"Server={uri.Host};Database={db};User Id={user};Password={passwd};Port={port};SSL Mode=Require;Trust Server Certificate=True;";
+    // Pooling=false ve Trust Server Certificate Render PostgreSQL için daha güvenlidir
+    connectionString = $"Server={uri.Host};Database={db};User Id={user};Password={passwd};Port={port};SSL Mode=Require;Trust Server Certificate=True;Pooling=false;";
     builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
 }
 
@@ -36,20 +40,23 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
+
 app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 
-// OTOMATÝK VERÝTABANI KURUCU
+// 3. KRÝTÝK DÜZELTME: Otomatik Migration ve Veritabaný Onarýmý
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
     try
     {
         var context = services.GetRequiredService<ApplicationDbContext>();
+        // Eðer tablo yapýsýnda SQLite'tan kalan bir uyumsuzluk varsa onarmaya çalýþýr
         context.Database.Migrate();
     }
     catch (Exception ex)
     {
-        Console.WriteLine("Hata: " + ex.Message);
+        Console.WriteLine("Veritabaný Hatasý: " + ex.Message);
     }
 }
+
 app.Run();
